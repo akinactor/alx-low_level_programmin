bec213ceb2753f053ccb788c045db3d0038ce882
Akinwale <akinactor@yahoo.com>
2023-02-22 10:15:23 -0800

    task two

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet - print lower case
+ */
+
+void print_alphabet(void)
+{
+char alph;
+for (alph = 'a'; alph <= z, alph++)
+_putchar(alph);
+_putchar('\n');
+}
