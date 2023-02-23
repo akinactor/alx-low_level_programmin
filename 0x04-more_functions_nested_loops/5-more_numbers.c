94011dcd427d55663ad64742bcf2d75f06ef8d42
Akinwale <akinactor@yahoo.com>
2023-02-23 08:45:04 -0800

    five more

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,19 +5,18 @@
  */
 void more_numbers(void)
 {
-int i = 0;
-while (i < 10)
+int i;
+for (i = 0; i < 10; i++)
 {
-int j;
-for (j = 0; j <= 14; j++)
+for (int j = 0; j <= 14; j++)
 {
-if (j > 0)
+if (j > 9)
 {
 _putchar((j / 10) + '0');
 }
 _putchar((j % 10) + '0');
 }
-i++;
+
 }
 _putchar('\n');
 return;
