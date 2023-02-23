b199c3676d820e2be3d8d2eab31092e49eccf429
Akinwale <akinactor@yahoo.com>
2023-02-23 03:18:52 -0800

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - Entry points for multiplication of numbers
+ * @a: the first int to multiply
+ * @b: the second int to multiply
+ * Return: the result
+ */
+int mul(int a, int b)
+{
+int result;
+result = a * b;
+
+return (result);
+
+
+}
