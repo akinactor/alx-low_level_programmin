f896141b8f41f04b9d194f0146db7254fca5e7e9
Akinwale <akinactor@yahoo.com>
2023-02-23 05:11:27 -0800

    ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - Entry point
+ * Return: return void
+ */
+void more_numbers(void)
+{
+int i = 0;
+while (i < 10)
+{
+int j;
+for (j = 0; j <= 14; j++)
+_putchar('0' + j);
+_putchar('\n');
+i++;
+}
+_putchar('\n');
+return;
+}
