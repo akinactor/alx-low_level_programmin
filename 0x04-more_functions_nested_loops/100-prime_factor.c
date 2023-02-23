1d142d5592b8cef5de6b45f9aeeb7954fc2021c8
Akinwale <akinactor@yahoo.com>
2023-02-23 11:36:59 -0800

    hundred

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <math.h>
+/**
+ * main - entry point
+ * Return: return 0
+ */
+int main(void)
+{
+
+long w;
+long maxi;
+long number = 612852475143;
+double square = sqrt(number);
+for (w = 1; w <= square; w++)
+{
+if (number % w == 0)
+	{
+	maxi = number / x;
+	}
+}
+printf("\ld\n", maxi);
+return (0);
+}
