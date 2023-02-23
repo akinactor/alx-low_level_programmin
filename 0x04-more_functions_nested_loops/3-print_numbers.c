141294123047054ce4ae30c4f84aa34d91daa8d0
Akinwale <akinactor@yahoo.com>
2023-02-23 03:46:25 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - Entry point of function print number
+ * Return: the return value is (0)
+ */
+void print_numbers(void)
+{
+int w;
+for (w = 0; w <= 9; w++)
+_putchar('0' + w);
+_putchar('\n');
+return;
+}
