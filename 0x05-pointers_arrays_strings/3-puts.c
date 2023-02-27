8bbae4da02402d7d1f6557200482110433d91a19
Akinwale <akinactor@yahoo.com>
2023-02-27 14:40:47 -0800

    3rd task

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts - entry point
+ * @str: the string to be printed
+ * Return: the return value is void
+ */
+void _puts(char *str)
+{
+
+while (*str != '\0')
+{
+_putchar(*str++);
+
+}
+
+return;
+
+
+}
