0a2db59c5e8c3c501e7ca63f2798e76fbd50d9ac
Akinwale <akinactor@yahoo.com>
2023-02-27 15:53:22 -0800

    sixth

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts2 - entry point
+ * @str: the string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+if ((i % 2) == 0)
+_putchar(str[i]);
+else
+continue;
+
+
+}
+_putchar('\n');
+return;
+
+}
