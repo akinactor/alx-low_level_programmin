5337801e332216e04fb4090b7d3d6a6d8c13dbb3
Akinwale <akinactor@yahoo.com>
2023-02-27 16:23:51 -0800

    seventh task

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * puts_half - entry point
+ * @str: string
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+int leng;
+
+while (*str != '\0')
+{
+leng++;
+str++;
+
+
+}
+str -= (leng / 2);
+while (*str != '\0')
+{
+_putchar(*str);
+
+str++;
+}
+_putchar('\n');
+
+}
