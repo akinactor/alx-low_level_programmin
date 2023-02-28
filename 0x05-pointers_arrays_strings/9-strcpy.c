21672795909b8cbfc9718afcd5d0714ab9486c58
Akinwale <akinactor@yahoo.com>
2023-02-27 17:13:56 -0800

    last

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,15 +8,15 @@
 
 char *_strcpy(char *dest, char *src)
 {
-int len = 0;
-while (*(src + len) != '\0')
+int leng = 0;
+while (*(src + leng) != '\0')
 {
-*(dest + len) = *(src + len);
-len++;
+*(dest + leng) = *(src + leng);
+leng++;
 
 
 }
-*(dest + len) = '\0';
+*(dest + leng) = '\0';
 return (dest);
 
 
